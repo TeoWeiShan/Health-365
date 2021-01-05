129a08e4397ddab9c1db8e752dc2e56eb2f18447
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-01-05 16:54:49 +0800

    Modified Query Display Information

--- js/recipe-script.js
+++ js/recipe-script.js
@@ -7,7 +7,7 @@ function getrecepe(q){
     for (var i=0; i<res.results.length;i++){
     document.getElementById("output").innerHTML+= 
     
-    "<h1>"+res.results[i].title+"</h1><br><img src='"+res.results[i].image+"' width='300' /><br>Ready in "+res.results[i].readyInMinutes+" minutes <br> Link to recipe: <a href='"+ res.results[i].sourceUrl+"'>Recipe Link </a> <br> "+res.results[i].nutrition.nutrients[0].title +": " +res.results[i].nutrition.nutrients[0].amount;
+    "<br><br><h1><a href='"+res.results[i].sourceUrl+"'>"+res.results[i].title+"</a></h1><br><img src='"+res.results[i].image+"' width='300' /><br>Ready in <b>"+res.results[i].readyInMinutes+" minutes</b><br> <br> " + res.results[i].summary;
     }
     }
     });
