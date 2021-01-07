e351cf3e8bcb808584e332fc487b710bcea7914f
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-01-08 00:43:21 +0800

    Validate Script

--- js/bmi-script.js
+++ js/bmi-script.js
@@ -1,24 +1,17 @@
 $(document).ready(function () {
-  
+
   displayTable();
+  displayChart();
 
   $("#frm-create-bmi").submit(function (e) {
-    
+
     e.preventDefault();
-    
-    var x = document.forms["myForm"]["weight"].value;
-    var y = document.forms["myForm"]["height"].value;
-    if (x == "" || y == "") {
-    alert("Cannot be left blank");
-  }
-  else{
 
     let weight = $('#weight').val();
     let height = $('#height').val();
     let createDate = new Date().toLocaleDateString();
     let userBMI = weight / (height * height) ;
     let userStatus = '';
-
     if (userBMI<18.5){
       userStatus = "Underweight";
     } 
@@ -44,7 +37,14 @@ $(document).ready(function () {
 
     localStorage.setItem('userList', JSON.stringify(userList));
 
-    displayTable();}
+    displayTable();
+
+    myChart.data.datasets[0].data.push(weight);
+
+    myChart.data.labels.push(createDate);
+
+    myChart.update();
+
   });
 });
 
@@ -54,7 +54,6 @@ function User(weight, height, createDate, userBMI, userStatus) {
   this.createDate = createDate;
   this.userBMI = userBMI;
   this.userStatus = userStatus;
-  
 }
 
 function displayTable() {
@@ -68,11 +67,58 @@ function displayTable() {
       for (let user of userList) {
         userInfo += `<tr><td>${user.userWeight}</td><td>${user.userHeight}</td><td>${user.createDate}</td><td>${user.userBMI}</td><td>${user.userStatus}</td></tr>`;
       }
-
       $('#user-info').html(userInfo);
     } else {
-      
+      $('#user-info').html('No user found');
     }
   }
-
 }
+
+function displayChart() {
+
+  if (localStorage.getItem('userList')) {
+    let userList = JSON.parse(localStorage.getItem('userList'));
+
+    if (userList.length > 0) {
+      for (let user of userList) {
+        myChart.data.datasets[0].data.push(user.userWeight);
+
+        myChart.data.labels.push(user.createDate);
+
+        myChart.update();
+      }
+    }
+  } 
+}
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Weight (kg)',
+      data: [],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)'
+      ],
+      borderWidth: 1
+    },
+  ]
+  },
+  options: {
+    title:{
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    }
+  }
+});
